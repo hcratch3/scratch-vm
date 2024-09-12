ded6ba6c73a6dd72df3ebaf31bd44761c7f99473
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-09-12 19:28:31 +0900

    Update index.js

--- src/extensions/scratch3_webgl/index.js
+++ src/extensions/scratch3_webgl/index.js
@@ -25,20 +25,38 @@ class Scratch3WebGLController {
         log.log('Sent background color change: ', color);
     }
 
-    // 球体などの立体を生成するブロック
+    // 名前付きの立体を生成するブロック
     createShape(args) {
+        const shapeName = Cast.toString(args.NAME);
         const shapeType = Cast.toString(args.SHAPE);
         const size = Cast.toNumber(args.SIZE);
-        this.channel.postMessage({type: 'createShape', shape: shapeType, size: size});
-        log.log('Sent create shape: ', shapeType, size);
+        this.channel.postMessage({type: 'createShape', name: shapeName, shape: shapeType, size: size});
+        log.log('Sent create shape: ', shapeName, shapeType, size);
     }
 
-    // 立体を移動させるブロック
+    // 立体の色を変更するブロック
+    setShapeColor(args) {
+        const shapeName = Cast.toString(args.NAME);
+        const color = Cast.toString(args.COLOR);
+        this.channel.postMessage({type: 'setShapeColor', name: shapeName, color: color});
+        log.log('Sent set shape color: ', shapeName, color);
+    }
+
+    // 名前付きの立体を移動させるブロック
     moveShape(args) {
+        const shapeName = Cast.toString(args.NAME);
         const direction = Cast.toString(args.DIRECTION);
         const distance = Cast.toNumber(args.DISTANCE);
-        this.channel.postMessage({type: 'moveShape', direction: direction, distance: distance});
-        log.log('Sent move shape: ', direction, distance);
+        this.channel.postMessage({type: 'moveShape', name: shapeName, direction: direction, distance: distance});
+        log.log('Sent move shape: ', shapeName, direction, distance);
+    }
+
+    // カメラの視点を移動させるブロック
+    moveCamera(args) {
+        const axis = Cast.toString(args.AXIS);
+        const amount = Cast.toNumber(args.AMOUNT);
+        this.channel.postMessage({type: 'moveCamera', axis: axis, amount: amount});
+        log.log('Sent move camera: ', axis, amount);
     }
 
     getInfo() {
@@ -60,12 +78,16 @@ class Scratch3WebGLController {
                 {
                     opcode: 'createShape',
                     blockType: BlockType.COMMAND,
-                    text: 'create [SHAPE] with size [SIZE]',
+                    text: 'create [SHAPE] named [NAME] with size [SIZE]',
                     arguments: {
                         SHAPE: {
                             type: ArgumentType.STRING,
                             menu: 'shapeMenu',
                             defaultValue: 'sphere'
+                        },
+                        NAME: {
+                            type: ArgumentType.STRING,
+                            defaultValue: 'myShape'
                         },
                         SIZE: {
                             type: ArgumentType.NUMBER,
@@ -74,16 +96,51 @@ class Scratch3WebGLController {
                     }
                 },
                 {
+                    opcode: 'setShapeColor',
+                    blockType: BlockType.COMMAND,
+                    text: 'set shape [NAME] color [COLOR]',
+                    arguments: {
+                        NAME: {
+                            type: ArgumentType.STRING,
+                            defaultValue: 'myShape'
+                        },
+                        COLOR: {
+                            type: ArgumentType.COLOR,
+                            defaultValue: '#ff0000' // 赤色
+                        }
+                    }
+                },
+                {
                     opcode: 'moveShape',
                     blockType: BlockType.COMMAND,
-                    text: 'move shape [DIRECTION] by [DISTANCE]',
+                    text: 'move shape [NAME] [DIRECTION] by [DISTANCE]',
                     arguments: {
+                        NAME: {
+                            type: ArgumentType.STRING,
+                            defaultValue: 'myShape'
+                        },
                         DIRECTION: {
                             type: ArgumentType.STRING,
                             menu: 'directionMenu',
                             defaultValue: 'up'
                         },
                         DISTANCE: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'moveCamera',
+                    blockType: BlockType.COMMAND,
+                    text: 'move camera [AXIS] by [AMOUNT]',
+                    arguments: {
+                        AXIS: {
+                            type: ArgumentType.STRING,
+                            menu: 'axisMenu',
+                            defaultValue: 'x'
+                        },
+                        AMOUNT: {
                             type: ArgumentType.NUMBER,
                             defaultValue: 1
                         }
@@ -98,6 +155,10 @@ class Scratch3WebGLController {
                 directionMenu: {
                     acceptReporters: true,
                     items: ['up', 'down', 'left', 'right', 'forward', 'backward']
+                },
+                axisMenu: {
+                    acceptReporters: true,
+                    items: ['x', 'y', 'z']
                 }
             }
         };
